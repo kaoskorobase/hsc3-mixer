e6cfcacbcf07b3be1d80901de1808e038d65ee89
Stefan Kersten <stefan.kersten@upf.edu>
2011-04-18 19:55:18 +0200

    Add bundle scheduling

--- mixer.hs
+++ mixer.hs
@@ -4,36 +4,51 @@ import Reactive hiding (accumulate)
 import Sound.SC3.Server.Reactive
 import Sound.SC3.Server.Resource
 import qualified Sound.SC3.Server.Command as C
-import Sound.OpenSoundControl (immediately)
+import Sound.OpenSoundControl (Time(..), immediately, pauseThreadUntil, utcr)
 import Control.Concurrent
 import Sound.SC3.Mixer
 
-playDefault = do
+playDefault t = do
+    let dur = 1
+        lat = 0.03
     r <- rootNode
-    synth <- async immediately $ s_new "default" AddToTail r []
+    synth <- async (UTCr (t+lat)) $ s_new "default" AddToTail r []
     fork $ do
-        liftIO $ threadDelay (truncate (1e6))
-        async immediately $ s_release 0 synth
+        let t' = t + dur
+        liftIO $ pauseThreadUntil t'
+        async (UTCr (t' + lat)) $ s_release 0 synth
     return ()
 
 dt = 0.0125
 
+inputLoop src t = do
+    fire src t
+    let t' = t + dt
+    pauseThreadUntil t'
+    inputLoop src t'
+
 mainR = do
     src <- newEventSource
-    forkIO $ forever $ do
-        fire src ()
-        threadDelay (truncate (dt * 1e6))
+    forkIO . inputLoop src =<< utcr
     withDefaultInternal $ do
-        b <- accumulate (\_ _ -> playDefault) () (fromEventSource src)
+    -- withDefaultSynth $ do
+        -- sync immediately $ send $ C.dumpOSC C.TextPrinter
+        b <- accumulate (\t _ -> playDefault t) () (fromEventSource src)
         liftIO $ reactimate $ fmap return $ changes b
         liftIO $ threadDelay (truncate (300e6))
 
+ioLoop t = do
+    playDefault t
+    let t' = t + dt
+    liftIO $ pauseThreadUntil t'
+    ioLoop t'
+
 mainIO = do
     withDefaultInternal $ do
-        sync immediately $ send $ C.dumpOSC C.TextPrinter
-        -- playDefault
-        mkStrip >>= liftIO . print
-        liftIO $ threadDelay (truncate (dt * 1000e6))
+    -- withDefaultSynth $ do
+        -- sync immediately $ send $ C.dumpOSC C.TextPrinter
+        -- mkStrip >>= liftIO . print
+        ioLoop =<< liftIO utcr
 
 main :: IO ()
 main = mainR
