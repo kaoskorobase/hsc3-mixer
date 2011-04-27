aedbeb17ec75aa228a09b5bd4736e65c32f18b25
Stefan Kersten <stefan.kersten@upf.edu>
2011-04-27 18:57:47 +0200

    Add missing module

---
+++ Sound/SC3/Mixer/SynthDefs.hs
@@ -0,0 +1,18 @@
+module Sound.SC3.Mixer.SynthDefs where
+
+import Sound.SC3
+
+mkInsertEffect :: Int -> (UGen -> UGen) -> UGen
+mkInsertEffect n f =
+    let bus = control KR "bus" 0
+        graph = f (in' n AR bus)
+        n' = length (mceChannels graph)
+    in if n' /= n
+       then error $ "mkInsertEffect: Channel mismatch: need " ++ show n ++ " got " ++ show n'
+       else replaceOut bus graph
+
+faderDef :: Int -> UGen
+faderDef n = mkInsertEffect n $ \i ->
+    let level = control KR "level" 1
+        mute = control KR "mute" 0
+    in i * level * (1 - mute)
